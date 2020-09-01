f9281e54c636902490a6f6268d5266d0c7fb0225
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-09-01 13:36:43 +0530

    signing out the users

--- scripts/auth.js
+++ scripts/auth.js
@@ -10,9 +10,17 @@ signupForm.addEventListener("submit", (e) => {
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
     const signup_modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(signup_modal).close();
     signupForm.reset();
+  });
+});
+
+// Log out method
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("User signed out");
   });
 });
