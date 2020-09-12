a0a5ea3eb98cd55a24edec6c2981879a54013bd0
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-09-12 12:47:29 +0530

    Added make admin ui and made one user admin by calling cloud function

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,14 @@
+// Add admin cloud function
+const adminForm = document.querySelector(".admin-actions");
+adminForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const adminEmail = document.querySelector("#admin-email").value;
+  const addAdminRole = functions.httpsCallable("addAdminRole");
+  addAdminRole({ email: adminEmail }).then((result) => {
+    console.log(result);
+  });
+});
+
 // Listen for auth status changes
 auth.onAuthStateChanged((user) => {
   if (user) {
